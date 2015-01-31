10e403cb824ab4a1665773e897491a741a1509c5
Devon Weller <dweller@devonweller.com>
2015-01-31 14:22:55 -0600

    simplified config

--- src/Tokenly/PusherClient/Provider/PusherClientServiceProvider.php
+++ src/Tokenly/PusherClient/Provider/PusherClientServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Tokenly\PusherClient\Provider;
 
 use Exception;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 /*
@@ -22,20 +23,33 @@ class PusherClientServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->package('tokenly/pusher-client', 'pusher-client', __DIR__.'/../../../');
+        $this->bindConfig();
 
         $this->app->bind('Nc\FayeClient\Client', function($app) {
-            $config = $app['config']['pusher-client::pusher'];
-            $client = new \Nc\FayeClient\Client(new \Nc\FayeClient\Adapter\CurlAdapter(), $config['serverUrl'].'/public');
+            $client = new \Nc\FayeClient\Client(new \Nc\FayeClient\Adapter\CurlAdapter(), Config::get('tokenlyPusher.serverUrl').'/public');
             return $client;
         });
 
         $this->app->bind('Tokenly\PusherClient\Client', function($app) {
-            $config = $app['config']['pusher-client::pusher'];
-            $client = new \Tokenly\PusherClient\Client($app->make('Nc\FayeClient\Client'), $config['password']);
+            $client = new \Tokenly\PusherClient\Client($app->make('Nc\FayeClient\Client'), Config::get('tokenlyPusher.password'));
             return $client;
         });
     }
+
+    protected function bindConfig() {
+        // simple config
+        $config = [
+            'tokenlyPusher.serverUrl' => getenv('PUSHER_SERVER_URL') ?: 'http://localhost:8000',
+            'tokenlyPusher.password'  => getenv('PUSHER_PASSWORD')   ?: null,
+        ];
+
+        // set the laravel config
+        Config::set($config);
+
+        return $config;
+    }
+
+
 
 }
 
